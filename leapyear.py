87870c77d212a2a25872801d284490d55dbdbc04
7karthy <36299114+7karthy@users.noreply.github.com>
2018-02-14 14:56:59 +0530

    Update leapyear.py

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,7 @@
 a=int(input("enter a year to find leap year or not"))
-if a%4 == 0 :
+if a <1:
+    print("invalid")
+elif a%4 == 0 :
     print("\nleap year")
 else:
     print("\nnot a leap year")
