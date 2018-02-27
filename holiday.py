6c559bf23b631fd56111915d2b1b3490fa02208d
7karthy <36299114+7karthy@users.noreply.github.com>
2018-02-27 17:01:37 +0530

    Create holiday.py

---
+++ holiday.py
@@ -0,0 +1,7 @@
+a=input("enter a day in a week\n")
+if(a== 'saturday' or a== 'sunday'):
+    print('Yes')
+elif(a== 'monday' or a== 'tuesday'or a== 'wednesday'or a== 'thursday'or a== 'friday'):
+    print("No")
+else:
+    print("invalid")
