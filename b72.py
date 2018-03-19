e5c9d2b5ac785796d589aed11466ff271de43f7d
7karthy <36299114+7karthy@users.noreply.github.com>
2018-03-19 17:22:43 +0530

    Create b72.py

---
+++ b72.py
@@ -0,0 +1,8 @@
+k=int(input('enter a number'))
+l=int(input('enter the start number'))
+r=int(input('enter the end number'))
+for i in range(l,r):
+    if k==i:
+        print('yes')
+    else:
+        continue
