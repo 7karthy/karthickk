375838c54e1e2e64cf1cca07e1a1e4eb65ebc80f
7karthy <36299114+7karthy@users.noreply.github.com>
2018-03-19 17:03:42 +0530

    Create vowelsinastring.py

---
+++ vowelsinastring.py
@@ -0,0 +1,7 @@
+k=(input('enter'))
+for i in k:
+  if (i=='a')or(i=='e')or(i=='i')or(i=='o')or(i=='u')or(i=='A')or(i=='E')or(i=='I')or(i=='O')or(i=='U'):
+    print("yes")
+    break
+  else:
+    continue
