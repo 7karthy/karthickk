c9a41d76f3e098ba818cd71f9e3a7ddd2aa17c0f
7karthy <36299114+7karthy@users.noreply.github.com>
2018-05-24 13:55:30 +0530

    Create b112.py

---
+++ b112.py
@@ -0,0 +1,10 @@
+a=int(input("enter the range"))
+d=input("enter the check value")
+k=[]
+for i in range(a):
+  c=input("enter the values")
+  k.append(c)
+if d in k:
+  print("yes")
+else:
+  print("no")
